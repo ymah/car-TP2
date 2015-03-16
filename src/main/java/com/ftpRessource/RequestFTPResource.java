cd4b5fec6ebb14bc09b4458571a92213a33646e8
Mahieddine Yaker <mahieddine.yaker@gmail.com>
2015-03-16 14:51:20 +0100

    commit

--- src/main/java/com/ftpRessource/RequestFTPResource.java
+++ src/main/java/com/ftpRessource/RequestFTPResource.java
@@ -3,12 +3,13 @@ package main.java.com.ftpRessource;
 import java.awt.PageAttributes.MediaType;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import main.java.FTPservice.FTPService;
-
+import javax.ws.rs.core.MediaType;
 
 
 @Path("/serverFTP")
